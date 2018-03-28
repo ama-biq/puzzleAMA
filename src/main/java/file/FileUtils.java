c65a5f59d517c30ab6f2e8bcfd0fffe234fd8e4f
Litvinsky, Andrey (al714h) <alt14h@intl.att.com>
2018-03-28 09:15:16 +0300

    [puzzle AMA] added method read file and return StringBuilder

--- src/main/java/file/FileUtils.java
+++ src/main/java/file/FileUtils.java
@@ -1,12 +1,43 @@
 package file;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileUtils {
 
-public static StringBuilder readFileToStringBuilder(File file){
-    StringBuilder stringBuilder = new StringBuilder();
-    return stringBuilder;
-}
+    public static StringBuilder readFileToStringBuilder(File file) {
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder;
+    }
 
+    public static StringBuilder readFile(File file, Charset encoding) throws Exception {
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+            String payload = new String(encoded, encoding);
+            return new StringBuilder(payload);
+        } catch (FileNotFoundException e) {
+            try {
+                throw new NoSuchFieldException(e.getMessage());
+            } catch (NoSuchFieldException e1) {
+                e1.printStackTrace();
+            }
+        } catch (IOException e) {
+            try {
+                throw new IOException();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        throw new Exception();
+    }
+
+
+    public static StringBuilder readFile(File file) throws Exception {
+        return readFile(file, StandardCharsets.UTF_8);
+    }
 }
--- src/test/java/file/FileParserTest.java
+++ src/test/java/file/FileParserTest.java
@@ -2,12 +2,16 @@ package file;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class FileParserTest {
 
 
     @Test
-    public void test(){
-        System.out.println("test");
+    public void test() throws Exception {
+        File file = new File("src\\test\\resources\\validPuzzle2Peaces.txt");
+        System.out.println(file.getAbsoluteFile());
+        FileUtils.readFile(file);
     }
 
 }
