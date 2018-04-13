940f8c6cb9321e372e64935c84d292cf1b32695d
Alexey Sandler <as371x@att.com>
2018-04-13 11:28:51 +0300

    Add new test files.

--- src/main/java/impl/Orchestrator.java
+++ src/main/java/impl/Orchestrator.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 public class Orchestrator {
     public static void main(String[] args) throws Exception {
-        File inputFile = new File("src\\test\\resources\\11AmirFileMatrix3on3.txt");
+        File inputFile = new File("src\\test\\resources\\18AmirFile.txt");
         Solver solver = new Solver();
         List<PuzzleElementDefinition> list = solver.checkTheInputFile(inputFile);
         if(list.isEmpty()){
             solver.writeErrorsToTheOutPutFile();
 
         }else if(solver.isSumOfEdgesZero(list)) {
+            //todo  check in loop corners, sum of all edges and straight edges
                 solver.solve(list);
         }
         if(solver.getSolutionList().isEmpty()){
             solver.writeErrorsToTheOutPutFile();
         }else{
             solver.writeSolutionToTheOutPutFile();
-           // solver.writeErrorsToTheOutPutFile();
         }
 
     }
